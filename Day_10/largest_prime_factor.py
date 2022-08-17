2617298762a3100b5dc76e98f5e96fccbdded79a
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2022-08-17 18:43:12 +0400

    Add files via upload

---
+++ Day_10/largest_prime_factor.py
@@ -0,0 +1,17 @@
+i = 3
+factors = []
+number = 600851475143
+while i < number:
+      if number % i != 0:
+         i += 2
+      else: 
+         number = number / i
+         factors.append(i)
+while number >= i:
+      if number % i != 0:
+         i += 2
+      else: 
+         number = number / i
+         factors.append(i)
+print(factors)
+     
