b345ced97e71ad4de2cdb2f3cf6871d64bd151f5
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2022-08-24 17:03:00 +0400

    Update largest_palindrome_prod.py

--- Day_10/largest_palindrome_prod.py
+++ Day_10/largest_palindrome_prod.py
@@ -1,11 +1,8 @@
-def is_palindrome(num):
-    while True:
-        str(num) == str(num)[::-1]
-def largest_palindrome(top, bot):
-    for i  in range(top, bot, -1):
-        for j in range(top, bot, -1):
-                num = i *j
-                if is_palindrome(num):
-                   return num
-print(largest_palindrome(999,100))
+prod = []
+for i in range(100, 999):
+    for j in range(100, 999):
+        num = i*j
+        if str(num) == str(num)[::-1]:
+            prod.append(num)
+print(max(prod))
 
